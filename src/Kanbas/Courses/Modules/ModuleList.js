84934ceb800655987f1a5320c4f4bccce1d0ccb4
Zongyu Wu <zongyuwu97@gmail.com>
2023-11-01 22:11:43 -0600

    a4

--- src/Kanbas/Courses/Modules/ModuleList.js
+++ src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./modulesReducer";
 
 function ModuleList() {
   const { courseId } = useParams();
-  const modules = db.modules;
+  const modules = useSelector((state) => state.modulesReducer.modules);
+  const module = useSelector((state) => state.modulesReducer.module);
+  const dispatch = useDispatch();
+
   return (
     <div className="d-flex flex-column">
       <div>
@@ -27,11 +36,34 @@ function ModuleList() {
       <hr />
 
       <ul className="list-group">
+        <li className="list-group-item ">
+          <button className="btn btn-success" onClick={() => dispatch(addModule({ ...module, course: courseId }))}>Add</button>
+          <button className="btn btn-primary" onClick={() => dispatch(updateModule(module))}>Update</button>
+          <input value={module.name}
+            onChange={(e) => dispatch(setModule({ ...module, name: e.target.value }))}
+          />
+          <textarea value={module.description}
+            onChange={(e) => dispatch(setModule({ ...module, description: e.target.value }))
+          }
+          />
+        </li>
         {
           modules
             .filter((module) => module.course === courseId)
             .map((module, index) => (
               <li key={index} className="list-group-item secondary" style={{ background: "#e2e3e5", marginTop: "30px" }}>
+                <button
+                  className="btn btn-success"
+                  onClick={() => dispatch(setModule(module))}>
+                  Edit
+                </button>
+
+                <button
+                  className="btn btn-danger"
+                  onClick={() => dispatch(deleteModule(module._id))}>
+                  Delete
+                </button>
+
                 <h3>{module.name}</h3>
                 <p>{module.description}</p>
                 {
